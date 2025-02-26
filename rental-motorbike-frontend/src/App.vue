ceeffc6dcb1ec28d25673ac4cbd2ed4581aa5f78
Manh Dao <manhdao1006@gmail.com>
2025-02-26 18:37:51 +0700

    create-fe

---
+++ rental-motorbike-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-view />
+</template>
+<script lang="ts">
+    import { defineComponent, provide, ref } from 'vue'
+
+    export default defineComponent({
+        name: 'App',
+        setup() {
+            const isLoggedIn = ref<boolean>(localStorage.getItem('isLoggedIn') === 'true')
+            provide('isLoggedIn', isLoggedIn)
+
+            return { isLoggedIn }
+        }
+    })
+</script>
